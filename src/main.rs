8759e337cddde8188bb0d83bfa61cac34970cce8
Lucas Ferreira <dummyconta1234@gmail.com>
2024-09-28 00:21:53 -0300

    Hello World getting started with Rust

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+
+
+fn main(){
+
+    let mut buffer = String::new();
+    println!("Enter Anything:");
+
+    std::io::stdin().read_line(&mut buffer).expect("Could Not Read Line");
+    
+    println!("Hello World")
+
+}
+

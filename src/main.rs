30399855a47ea80a8460dfc20ad8845f846226e6
Lucas Ferreira <dummyconta1234@gmail.com>
2024-10-01 01:28:30 -0300

    Added Basic Logic For Adding Float Valued Symbols

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 
-
-
 enum AtomType {
-    LiteralNone,
     LiteralI64,
     LiteralF64
 }
 
 
 enum SymbolTypes {
-    SymbolNone,
     SymbolAtom(AtomType)
 }
 
-
-
 struct Symbol{
     name: String,
-    symbol_type: SymbolTypes
+    symbol_type: SymbolTypes,
+    data: Option<usize>,
 }
 
+fn get_type_string(input: &SymbolTypes) -> Option<String> {
+    return match input {
+        SymbolTypes::SymbolAtom(AtomType::LiteralI64) => Some(String::from("i64")),
+        SymbolTypes::SymbolAtom(AtomType::LiteralF64) => Some(String::from("f64")),
+        //_ => None
+    }
+}
 
 
 fn get_type(input: &str) -> Option<SymbolTypes> {
@@ -31,29 +33,101 @@ fn get_type(input: &str) -> Option<SymbolTypes> {
     }
 }
 
+fn parse_daclaration(argv: &std::vec::Vec<&str>, data_buffer: &mut std::vec::Vec<f64>) -> Option<Symbol> {
+    if argv.len() < 2 || argv.len() == 3{
+        return None;
+    } else if argv.len() == 2 {
+        return Some(Symbol {
+            name: argv[1].to_string(),
+            symbol_type: get_type(argv[0]).expect("Expected Identifier"),
+            data: None
+        });
+    }
+    let value: f64 = argv[3].parse().expect("Expected Expression");
+    let data_pos = data_buffer.len();
+    data_buffer.push(value);
+    return Some(Symbol {
+        name: argv[1].to_string(),
+        symbol_type: get_type(argv[0]).expect("Expected Identifier"),
+        data: Some(data_pos)
+    });
+}
+
+
+fn display_symbol(symbol_str: &str, symbol_table: &std::vec::Vec<Symbol>, data_buffer: &std::vec::Vec<f64>){
+
+
+    let mut symbol_found: Option<&Symbol> = None;
+
+    for symb in symbol_table {
+        if symbol_str == symb.name {
+            symbol_found = Some(symb);
+            break;
+        }
+    }
+
+    let symbol: &Symbol = symbol_found.expect("Symbol Not Found");
+
+    
+
+    if Option::is_some(&symbol.data) {
+        println!(
+            "Symbol: {symb_type} {name} = {value}",
+            symb_type=get_type_string(&symbol.symbol_type).expect("Invalid Type"),
+            name=symbol.name, value=data_buffer[symbol.data.expect("Expected Data")]
+        );
+    } else {
+        println!(
+            "Symbol: {symb_type} {name}",
+            symb_type=get_type_string(&symbol.symbol_type).expect("Invalid Type"),
+            name=symbol.name
+        );
+    }
+
+}
+
 
 fn main(){
 
     println!("Enter Anything:");
 
-    let mut buffer = String::new();
+    let mut input_buffer = String::new();
 
-    std::io::stdin().read_line(&mut buffer).expect("Could Not Read Line");
+    let mut data_buffer: std::vec::Vec<f64> = vec![];
 
-    let tokens: std::vec::Vec<&str> = buffer.split_whitespace().collect();
+    let mut symbols: std::vec::Vec<Symbol> = vec![];
 
-    let id = tokens[0];
+    let mut i: u8 = 0;
 
-    match id {
-        
-        "i64" => println!("64 bits integer"),
-        "f64" => println!("64 bits float"),
+    while i < 5 {
 
-        _ => println!("Unknown Type"),
+        i+=1;
+
+        input_buffer.clear();
+
+        std::io::stdin().read_line(&mut input_buffer).expect("Could Not Read Line");
+
+        let tokens: std::vec::Vec<&str> = input_buffer.split_whitespace().collect();
+
+        if tokens.len() == 0 || tokens[0] == "close" {
+            break;
+        }
+        if tokens[0] == "show" {
+            if tokens.len() == 1 {
+                println!("Expected Symbol");
+                std::process::exit(1);
+            }
+            display_symbol(tokens[1], &symbols, &data_buffer);
+            continue;
+        }
+
+        let symbol: Symbol = parse_daclaration(&tokens, &mut data_buffer).expect("Invalid Expression");
+
+        symbols.push(symbol);
         
     }
+
     
-    println!("Hello World")
 
 }
 
